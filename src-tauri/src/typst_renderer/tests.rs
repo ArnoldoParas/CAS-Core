7a9e2c27367097b61cffad33aa2e1524ff47e618
Arnoldo Arturo Paras Reyes <aparasreyes2@gmail.com>
2025-03-31 23:35:33 -0700

    Add typst tests

---
+++ src-tauri/src/typst_renderer/tests.rs
@@ -0,0 +1,18 @@
+use super::{generate_pdf, Data, LabelData, LabelStyle};
+use std::fs;
+
+
+
+#[test]
+fn label_test() {
+
+  println!("Directorio de trabajo actual: {:?}", std::env::current_dir().unwrap());
+  let data = LabelData {
+    style: LabelStyle::Type1,
+    dependence: "FIME".to_string(),
+    amount: 20,
+    start: 1,
+  };
+
+  generate_pdf(Data::Label(data)).unwrap();
+}
